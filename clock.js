896045eba3a98997d585da79475397c642e14d21
eunhagfriend <48969007+santorini19970530@users.noreply.github.com>
2021-08-06 22:42:26 +0800

    Update clock.js

--- clock.js
+++ clock.js
@@ -2,7 +2,8 @@ function keepTheClockMove()
 {
 	var n = new Date(); // new Date() is pre-set to be current date
 
-	// hour : miniute : second
+	// hour : miniute : second (24-hr system, with always two digits)
+	// japanese ponunciation of "minute" will be differecnt, depends on the digit in front of it
 	let minuteSuffix = ["ぷん","ふん","ぷん","ぷん","ふん","ぷん","ぷん","ふん","ふん","ぷん"];
 
 	document.getElementById("hour").innerHTML = (n.getHours()).toLocaleString('ja-jp', {minimumIntegerDigits: 2, useGrouping:false});
@@ -10,8 +11,9 @@ function keepTheClockMove()
 	document.getElementById("minuteCaption").innerHTML = minuteSuffix[(n.getMinutes()+1)%10];
 	document.getElementById("second").innerHTML = (n.getSeconds()).toLocaleString('ja-jp', {minimumIntegerDigits: 2, useGrouping:false});
 
-	// date month year (weekday)
+	// date month year weekday
 	document.getElementById("dateDIV").innerHTML = n.toLocaleString('ja-jp', {year: "numeric", month: "long", day: "numeric", weekday: "long"});
 
+	// this function will restarted every 1 second.
 	setTimeout(keepTheClockMove, 1000);
 }
